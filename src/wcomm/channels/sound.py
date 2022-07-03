06282ebcabeaeb3b4159a4fbe4570aba0c00880c
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-03 05:50:04 -0400

    Implemented the listen method

--- src/wcomm/channels/sound.py
+++ src/wcomm/channels/sound.py
@@ -1,9 +1,23 @@
 import winsound
+from time import sleep
 from wcomm.channel import Channel
 from wcomm.utils.log import log
+from pysinewave import SineWave
 
 
 DEFAULT_SOUND_TIME = 1000
+sine = None
+
+def set_sine_freq(freq):
+    global sine
+    if sine is None:
+        sine = SineWave(pitch_per_second=10000)
+        sine.set_frequency(freq)
+        sine.play()
+        return sine
+    else:
+        sine.set_frequency(freq)
+        return sine
 
 
 class SoundChannel(Channel):
@@ -17,4 +31,9 @@ class SoundChannel(Channel):
 
     def play(self, frequency, time=DEFAULT_SOUND_TIME):
         log(f"PLAY {frequency} Hz , {time} ms\n")
-        winsound.Beep(frequency, time)
+        # winsound.Beep(frequency, time)
+        set_sine_freq(frequency)
+        sleep(time / 1000)
+
+    def listen(self, frequency):
+        self._modulator.listen(frequency)
