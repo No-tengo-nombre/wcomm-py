0681a5e7b0c7542b28f5211121eeed865d6df25e
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-03 16:49:11 -0400

    Fixed the example

--- src/wcomm/examples/detection_test/main.py
+++ src/wcomm/examples/detection_test/main.py
@@ -3,16 +3,20 @@ import numpy as np
 
 
 def main():
-    num = 10000
-    
+    num = 100
+
     mod = FSK256()
     test_axis1 = np.cos(
-        2 * np.pi * 300 * mod._sampling_frequency * np.arange(num))
+        2 * np.pi * 300 * np.arange(num) / mod._sampling_frequency
+    )
     test_axis1_phased = np.cos(
-        2 * np.pi * 300 * mod._sampling_frequency * np.arange(num) + 1)
+        2 * np.pi * 300 * np.arange(num) / mod._sampling_frequency + 1
+    )
     test_axis2 = np.cos(
-        2 * np.pi * 650 * mod._sampling_frequency * np.arange(num))
+        2 * np.pi * 650 * np.arange(num) / mod._sampling_frequency
+    )
     test_axis2_phased = np.cos(
-        2 * np.pi * 650 * mod._sampling_frequency * np.arange(num) + 1)
+        2 * np.pi * 650 * np.arange(num) / mod._sampling_frequency + 1
+    )
 
     print(f"AXIS 1 -> {mod.detect(test_axis1)} , {mod.detect(test_axis1_phased)}\nAXIS 2 -> {mod.detect(test_axis2)} , {mod.detect(test_axis2_phased)}")
