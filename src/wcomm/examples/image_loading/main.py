a21bd4641c7188794c236ae8b1c4c5f2ca8771f3
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-07 02:53:24 -0400

    Added an example to test image loading

--- src/wcomm/examples/image_loading/main.py
+++ src/wcomm/examples/image_loading/main.py
@@ -1,8 +1,16 @@
-import numpy as np
-import cv2
+from wcomm.message import Message
+from wcomm.encoding.source.huffman import HuffmanCode
 
 
 def main():
-    data = cv2.imread("wcomm/examples/resources/img_house.jpeg")
-    data_arr = np.array(data)
-    print(data_arr)
+    message_r = Message.from_image("wcomm/examples/resources/img/img_house.jpeg", 0)
+    message_g = Message.from_image("wcomm/examples/resources/img/img_house.jpeg", 1)
+    message_b = Message.from_image("wcomm/examples/resources/img/img_house.jpeg", 2)
+
+    print(message_r.bit_size() + message_g.bit_size() + message_b.bit_size())
+
+    coding_r = HuffmanCode.from_message(message_r)
+    coding_g = HuffmanCode.from_message(message_g)
+    coding_b = HuffmanCode.from_message(message_b)
+    
+    print(coding_r.encode(message_r).bit_size() + coding_g.encode(message_g).bit_size() + coding_b.encode(message_b).bit_size())
