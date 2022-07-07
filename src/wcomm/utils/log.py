bf697a8af54d9dee6bf33bac3d19d74c6f799652
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-07 01:30:40 -0400

    Added a function to add a metric prefix

--- src/wcomm/utils/log.py
+++ src/wcomm/utils/log.py
@@ -1,6 +1,11 @@
 from wcomm import WCOMM_CONFIG
+from quantiphy import Quantity
 
 
 def log(message):
     if WCOMM_CONFIG["verbose"]:
         print(message)
+
+
+def metric_prefix(quantity, prefix):
+    return str(Quantity(quantity, prefix))
