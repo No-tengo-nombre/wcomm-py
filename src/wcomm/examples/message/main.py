04f39d34fd0b989d73155f46ec4f09be99e121eb
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-06 23:16:43 -0400

    Added another test message

--- src/wcomm/examples/message/main.py
+++ src/wcomm/examples/message/main.py
@@ -4,6 +4,11 @@ from wcomm.message import Message
 def main():
     test1 = Message("010011")
     test2 = Message.from_binary("010011")
+    test3 = Message("""this
+is
+    a
+    test""")
 
     print(test1)
     print(test2)
+    print(test3)
