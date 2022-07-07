067869e08197301f4bfb716d7cc6ee809bb98e0e
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-07 13:03:08 -0400

    Added loading images as YUV

--- src/wcomm/examples/image_loading/main.py
+++ src/wcomm/examples/image_loading/main.py
@@ -1,8 +1,7 @@
 from wcomm.message import Message
 from wcomm.encoding.source.huffman import HuffmanCode
 from wcomm.utils.log import metric_prefix
-from skimage.color import rgb2hsv
-import cv2
+from skimage.color import rgb2hsv, rgb2yuv
 import numpy as np
 
 
@@ -34,3 +33,18 @@ def main():
     hsv_cd_v = HuffmanCode.from_message(hsv_msg_v)
     
     print(f"HSV After encoding -> {metric_prefix(hsv_cd_h.encode(hsv_msg_h).bit_size() + hsv_cd_s.encode(hsv_msg_s).bit_size() + hsv_cd_v.encode(hsv_msg_v).bit_size(), 'b')}")
+
+
+    # Reading the data in YUV
+    yuv_msg_y = Message.from_raw_image("wcomm/examples/resources/img/img_house.jpeg", 0, lambda data: (255 * rgb2yuv(data)).astype(np.uint8))
+    yuv_msg_u = Message.from_raw_image("wcomm/examples/resources/img/img_house.jpeg", 1, lambda data: (255 * rgb2yuv(data)).astype(np.uint8))
+    yuv_msg_v = Message.from_raw_image("wcomm/examples/resources/img/img_house.jpeg", 2, lambda data: (255 * rgb2yuv(data)).astype(np.uint8))
+
+
+    print(f"YUV Before encoding -> {metric_prefix(yuv_msg_y.bit_size() + yuv_msg_u.bit_size() + yuv_msg_v.bit_size(), 'b')}")
+
+    yuv_cd_y = HuffmanCode.from_message(yuv_msg_y)
+    yuv_cd_u = HuffmanCode.from_message(yuv_msg_u)
+    yuv_cd_v = HuffmanCode.from_message(yuv_msg_v)
+
+    print(f"YUV After encoding -> {metric_prefix(yuv_cd_y.encode(yuv_msg_y).bit_size() + yuv_cd_u.encode(yuv_msg_u).bit_size() + yuv_cd_v.encode(yuv_msg_v).bit_size(), 'b')}")
