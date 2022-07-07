7e74b9bcd1d1d605c4e247a94710e958f0138b4f
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-07 01:31:24 -0400

    Added methods to the abstract class

--- src/wcomm/encoding/source/source_coding.py
+++ src/wcomm/encoding/source/source_coding.py
@@ -1,8 +1,33 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractstaticmethod
 
 
 class SourceCoding(ABC):
     """Abstract class for a source coding scheme."""
+    __EMPTY_CHAR_MAPPING = {key: 0 for key in range(256)}
+
+    @classmethod
+    def from_message(cls, message):
+        return cls(code=cls.generate_code(message))
+
+    def update_code(self, message):
+        self._code = self.generate_code(message)
+
+    @staticmethod
+    def empty_map():
+        return SourceCoding.__EMPTY_CHAR_MAPPING.copy()
+
+    @staticmethod
+    def generate_frequency_map(message):
+        output = SourceCoding.empty_map()
+        for c in message.as_string():
+            output[ord(c)] += 1
+        return output
 
     @abstractmethod
     def encode(self, message): pass
+
+    @abstractmethod
+    def decode(self, message): pass
+
+    @abstractstaticmethod
+    def generate_code(message): pass
