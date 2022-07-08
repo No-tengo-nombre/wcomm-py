b75f47294a318a262d5a308f3394cd4d3d7cd2cd
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-08 14:37:44 -0400

    Added methods to send the modulation's name as a header in the message

--- src/wcomm/modulation/modulation.py
+++ src/wcomm/modulation/modulation.py
@@ -1,3 +1,5 @@
+from wcomm.utils.log import log
+from wcomm.message import Message
 from abc import ABC, abstractmethod
 
 
@@ -7,11 +9,20 @@ class Modulator(ABC):
     @abstractmethod
     def get_name(self): pass
 
+    def get_name_message(self):
+        return Message(self.get_name())
+
     @abstractmethod
     def split(self, message): pass
 
+    def send_name(self, channel, time):
+        log(f"INFO::SEND MODULATION HEADER")
+        for key in self.split(self.get_name_message()):
+            log(f"INFO::SEND CHAR '{chr(key)}' = {key}")
+            channel.play(self.calculate_frequency(key), time)
+
     @abstractmethod
-    def send_through_channel(self, channel, message, time): pass
+    def send_through_channel(self, channel, message, time, send_header=True): pass
 
     @abstractmethod
     def detect(self, data, samp_freq): pass
