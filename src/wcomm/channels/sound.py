0e10e52532c5480fca5f494a63e5465c8eae5f24
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-07-08 14:49:06 -0400

    Removed unused code

--- src/wcomm/channels/sound.py
+++ src/wcomm/channels/sound.py
@@ -3,85 +3,14 @@ import numpy as np
 from time import sleep
 from wcomm.channels.channel import Channel
 from wcomm.utils.log import log
-import winsound
-from tones import SINE_WAVE
-from tones.mixer import Mixer
 from pysinewave import SineWave
 from multipledispatch import dispatch
 from numbers import Number
-
-import pyaudio
-
-# import pygame as pg
-
-
-
-
-class ToneGenerator(object):
- 
-    def __init__(self, samplerate=44100, frames_per_buffer=220):
-        self.p = pyaudio.PyAudio()
-        self.samplerate = samplerate
-        self.frames_per_buffer = frames_per_buffer
-        self.streamOpen = False
- 
-    def sinewave(self):
-        if self.buffer_offset + self.frames_per_buffer - 1 > self.x_max:
-            # We don't need a full buffer or audio so pad the end with 0's
-            xs = np.arange(self.buffer_offset,
-                              self.x_max)
-            tmp = self.amplitude * np.sin(xs * self.omega)
-            out = np.append(tmp,
-                               np.zeros(self.frames_per_buffer - len(tmp)))
-        else:
-            xs = np.arange(self.buffer_offset,
-                              self.buffer_offset + self.frames_per_buffer)
-            out = self.amplitude * np.sin(xs * self.omega)
-        self.buffer_offset += self.frames_per_buffer
-        return out
- 
-    def callback(self, in_data, frame_count, time_info, status):
-        if self.buffer_offset < self.x_max:
-            data = self.sinewave().astype(np.float32)
-            return (data.tostring(), pyaudio.paContinue)
-        else:
-            return (None, pyaudio.paComplete)
- 
-    def is_playing(self):
-        if self.stream.is_active():
-            return True
-        else:
-            if self.streamOpen:
-                self.stream.stop_stream()
-                self.stream.close()
-                self.streamOpen = False
-            return False
- 
-    def play(self, frequency, duration, amplitude):
-        self.omega = float(frequency) * (np.pi * 2) / self.samplerate
-        self.amplitude = amplitude
-        self.buffer_offset = 0
-        self.streamOpen = True
-        self.x_max = np.ceil(self.samplerate * duration) - 1
-        self.stream = self.p.open(format=pyaudio.paFloat32,
-                                  channels=1,
-                                  rate=self.samplerate,
-                                  output=True,
-                                  frames_per_buffer=self.frames_per_buffer,
-                                  stream_callback=self.callback)
-
-
-
 
 DEFAULT_BLOCKSIZE = 441
 DEFAULT_THRESHOLD = 20
 DEFAULT_SOUND_TIME = 1000
 sine = None
-mixer = Mixer(44100, 1)
-mixer.create_track(0, SINE_WAVE)
-generator = ToneGenerator()
-
-# pg_mixer = pg.mixer.init(44100, -16, 1, 512)
 
 
 def set_sine_freq(freq):
@@ -111,10 +40,6 @@ class SoundChannel(Channel):
 
         set_sine_freq(frequency)
         sleep(time / 1000)
-
-        # generator.play(frequency, time / 1000, 1)
-        # while generator.is_playing():
-        #     pass
 
     def start_microphone(self, sample_rate=None, channels=1):
         if sample_rate is None:
